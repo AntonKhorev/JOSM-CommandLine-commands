7431bbcc496e00a73b5e8e3012b637c99d9527d3
Anton Khorev <tony29@yandex.ru>
2011-08-08 03:13:06 +0400

    correct arg count

--- proportion.py
+++ proportion.py
@@ -7,12 +7,12 @@ import projections
 from OsmData import OsmData
 
 def main():
-	if len(sys.argv) != 3:
-		return 0
-	tData = OsmData()
+	if len(sys.argv)!=4:
+		 return 0
+	tData=OsmData()
 	# tData.addcomment("Done.")
 	tData.addcomment("Hello World.")
 	tData.write(sys.stdout)
 
-if __name__ == '__main__':
+if __name__=='__main__':
 	main()
