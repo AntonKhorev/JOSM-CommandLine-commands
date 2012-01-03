8176e29192f1d620c8363e553f280545e0e07c4e
Anton Khorev <tony29@yandex.ru>
2012-01-04 00:33:55 +0300

    waytoway handles both sides of continued way

--- waytoway.py
+++ waytoway.py
@@ -18,30 +18,37 @@ def main():
 		refdata.nodes[id] for id in wu[OsmData.REF]
 	]]
 
-	pc1=wcpts[0]
-	pc2=wcpts[1]
+	resultdata=osmcmd.Data()
+	if wcid==wuid:
+		resultdata.write('Same way')
+		return
+
 	mi=None
 	ml=float('inf')
-	for i,pu1,pu2 in [(i,wupts[i],wupts[i+1]) for i in range(len(wupts)) if i<len(wupts)-1]:
-		det =(pc2.x-pc1.x)*(pu2.y-pu1.y)-(pu2.x-pu1.x)*(pc2.y-pc1.y)
-		detl=(pc1.x-pu1.x)*(pu2.y-pu1.y)-(pu2.x-pu1.x)*(pc1.y-pu1.y)
-		dets=(pc2.x-pc1.x)*(pc1.y-pu1.y)-(pc1.x-pu1.x)*(pc2.y-pc1.y)
-		l=detl/det
-		s=dets/det
-		if s<0 or s>1:
-			continue
-		if l<ml:
-			ml=l
-			mi=i
+	mf=None
+	for f,pc1,pc2 in ((True,wcpts[0],wcpts[1]),(False,wcpts[-1],wcpts[-2])):
+		for i,pu1,pu2 in [(i,wupts[i],wupts[i+1]) for i in range(len(wupts)) if i<len(wupts)-1]:
+			det =(pc2.x-pc1.x)*(pu2.y-pu1.y)-(pu2.x-pu1.x)*(pc2.y-pc1.y)
+			detl=(pc1.x-pu1.x)*(pu2.y-pu1.y)-(pu2.x-pu1.x)*(pc1.y-pu1.y)
+			dets=(pc2.x-pc1.x)*(pc1.y-pu1.y)-(pc1.x-pu1.x)*(pc2.y-pc1.y)
+			l=detl/det
+			s=dets/det
+			if s<0 or s>1:
+				continue
+			if l<ml:
+				ml=l
+				mi=i
+				mf=f
 
-	td=osmcmd.Data()
 	if mi is None:
-		td.write('Impossible')
-	else:
-		#td.write('Intersects after pt '+str(mi))
-		pi=pc1+(pc1-pc2)*ml
-		td.addNode(pi)
-		td.write()
+		resultdata.write('Impossible')
+		return
+
+	#resultdata.write('Intersects after pt '+str(mi))
+	pc1,pc2 = (wcpts[0],wcpts[1]) if mf else (wcpts[-1],wcpts[-2])
+	pi=pc1+(pc1-pc2)*ml
+	resultdata.addNode(pi)
+	resultdata.write()
 
 if __name__=='__main__':
 	main()
