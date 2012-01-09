2a3b7d6d8380d5064ff30c6151e181c200958dce
Anton Khorev <tony29@yandex.ru>
2012-01-09 22:05:35 +0300

    select way and nodes together to make a stop

--- ptmakestop.py
+++ ptmakestop.py
@@ -7,16 +7,26 @@ import osmcmd
 import shootnodetoway
 
 def main():
-	try:
-		data,id1,id2=shootnodetoway.sntw()
-		data.nodes[id1][OsmData.ACTION]=OsmData.MODIFY
-		data.nodes[id1][OsmData.TAG].update({'public_transport':'platform'})
-		data.nodes[id2][OsmData.TAG].update({'public_transport':'stop_position'})
-		data.addcomment('Done')
+	data=osmcmd.readData()
+	nwdata=osmcmd.readData()
+	data.mergedata(nwdata)
+	wayid=next(iter(nwdata.ways))
+	n=0
+	for platformid in nwdata.nodes:
+		stopid=shootnodetoway.sntw(data,platformid,wayid)
+		if stopid is None:
+			continue
+		n+=1
+		data.nodes[platformid][OsmData.ACTION]=OsmData.MODIFY
+		data.nodes[platformid][OsmData.TAG].update({'public_transport':'platform'})
+		data.nodes[stopid][OsmData.TAG].update({'public_transport':'stop_position'})
+
+	if n>0:
+		data.addcomment(str(n)+' stops created')
 		data.write(sys.stdout)
-	except:
+	else:
 		resultdata=osmcmd.Data()
-		resultdata.write('Impossible')
+		resultdata.write('WARNING: no stops created')
 
 if __name__=='__main__':
 	main()
--- shootnodetoway.py
+++ shootnodetoway.py
@@ -5,32 +5,24 @@ import sys
 import osmcmd
 import OsmData
 
-def sntw():
-	data=osmcmd.readData()
-	ncdata=osmcmd.readData()
-	ncid=next(iter(ncdata.nodes))
-	data.mergedata(ncdata)
-	wudata=osmcmd.readData()
-	wuid=next(iter(wudata.ways))
-	data.mergedata(wudata)
-
-	nc=data.nodes[ncid]
-	ncpt=osmcmd.Point('latlon',nc[OsmData.LAT],nc[OsmData.LON])
-	wu=data.ways[wuid]
-	wupts=[osmcmd.Point('latlon',node[OsmData.LAT],node[OsmData.LON]) for node in [
-		data.nodes[id] for id in wu[OsmData.REF]
+def sntw(data,nodeid,wayid):
+	node=data.nodes[nodeid]
+	nodept=osmcmd.Point('latlon',node[OsmData.LAT],node[OsmData.LON])
+	way=data.ways[wayid]
+	waypts=[osmcmd.Point('latlon',waynode[OsmData.LAT],waynode[OsmData.LON]) for waynode in [
+		data.nodes[id] for id in way[OsmData.REF]
 	]]
 
 	mi=None
 	ml=float('inf')
 	ms=float('inf')
-	for i,pu1,pu2 in [(i,wupts[i],wupts[i+1]) for i in range(len(wupts)) if i<len(wupts)-1]:
-		pc1=ncpt
-		dpu=pu2-pu1
-		pc2=pc1+osmcmd.Vector(-dpu.y,dpu.x) # fake point to make perpendicular line
+	for i,wp1,wp2 in [(i,waypts[i],waypts[i+1]) for i in range(len(waypts)-1)]:
+		np1=nodept
+		dwp=wp2-wp1
+		np2=np1+osmcmd.Vector(-dwp.y,dwp.x) # fake point to make perpendicular line
 		l,s=osmcmd.solveLinEqns((
-			(pc2.x-pc1.x,pu2.x-pu1.x,pc1.x-pu1.x),
-			(pc2.y-pc1.y,pu2.y-pu1.y,pc1.y-pu1.y),
+			(np2.x-np1.x,wp2.x-wp1.x,np1.x-wp1.x),
+			(np2.y-np1.y,wp2.y-wp1.y,np1.y-wp1.y),
 		))
 		if s<0 or s>1:
 			continue
@@ -40,26 +32,33 @@ def sntw():
 			ms=s
 
 	if mi is None:
-		raise Exception('Impossible')
+		return None
 
-	pu1,pu2=wupts[mi],wupts[mi+1]
-	pi=pu1+(pu2-pu1)*ms
+	wp1,wp2=waypts[mi],waypts[mi+1]
+	pi=wp1+(wp2-wp1)*ms
 	id=data.addnode()
 	data.nodes[id][OsmData.LON]=pi.lon
 	data.nodes[id][OsmData.LAT]=pi.lat
 	data.nodes[id][OsmData.TAG]={}
-	data.ways[wuid][OsmData.ACTION]=OsmData.MODIFY
-	data.ways[wuid][OsmData.REF].insert(mi+1,id)
-	return data,ncid,id
+	data.ways[wayid][OsmData.ACTION]=OsmData.MODIFY
+	data.ways[wayid][OsmData.REF].insert(mi+1,id)
+	return id
 
 def main():
-	try:
-		data,id1,id2=sntw()
-		data.addcomment('Done')
-		data.write(sys.stdout)
-	except:
+	data=osmcmd.readData()
+	nodedata=osmcmd.readData()
+	nodeid=next(iter(nodedata.nodes))
+	data.mergedata(nodedata)
+	waydata=osmcmd.readData()
+	wayid=next(iter(waydata.ways))
+	data.mergedata(waydata)
+	id=sntw(data,nodeid,wayid)
+	if id is None:
 		resultdata=osmcmd.Data()
 		resultdata.write('Impossible')
+	else:
+		data.addcomment('Done')
+		data.write(sys.stdout)
 
 if __name__=='__main__':
 	main()
