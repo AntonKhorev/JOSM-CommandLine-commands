510019128aea9427fab25fe570195bc99862ad6e
Anton Khorev <tony29@yandex.ru>
2012-02-09 08:00:13 +0300

    MakeStopArea propagates name to stops/platforms

--- ptmakestoparea.py
+++ ptmakestoparea.py
@@ -8,6 +8,7 @@ import osmcmd
 def main():
 	data=osmcmd.readData()
 	nodedata=osmcmd.readData()
+	data.mergedata(nodedata)
 
 	while data.relations.get(data.currrelationid)!=None:
 		data.currrelationid-=1
@@ -23,13 +24,23 @@ def main():
 			'public_transport':'stop_area',
 		}
 	}
+	name=None
 	for id,node in nodedata.nodes.items():
+		if 'name' in node[OsmData.TAG]:
+			name=node[OsmData.TAG]['name']
+	if name is not None:
+		rel[OsmData.TAG]['name']=name
+	for id in nodedata.nodes:
+		node=data.nodes[id]
 		if node[OsmData.TAG].get('public_transport')=='stop_position':
 			rel[OsmData.REF][OsmData.NODES].append((id,'stop'))
 		elif node[OsmData.TAG].get('public_transport')=='platform':
 			rel[OsmData.REF][OsmData.NODES].append((id,'platform'))
-		if 'name' in node[OsmData.TAG]:
-			rel[OsmData.TAG]['name']=node[OsmData.TAG]['name']
+		else:
+			continue
+		if name is not None and 'name' not in node[OsmData.TAG]:
+			node[OsmData.ACTION]=OsmData.MODIFY
+			node[OsmData.TAG]['name']=name
 
 	if len(rel[OsmData.REF][OsmData.NODES])<=0:
 		osmcmd.fail('No stops/platforms provided')
