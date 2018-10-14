707aa44a243d20ca452384964a0e1c3f10987764
Anton Khorev <tony29@yandex.ru>
2018-10-14 15:01:50 +0300

    some error checking in CornerRestore

--- cornerrestore.py
+++ cornerrestore.py
@@ -10,6 +10,8 @@ def main():
 	opdata=osmcmd.readData()
 	data.mergedata(opdata)
 	cornerNids=set(opdata.nodes)
+	if len(opdata.ways)<1:
+		data.addcomment('No ways to operate on')
 	for wid in opdata.ways:
 		way=opdata.ways[wid]
 		wayNids=way[OsmData.REF]
@@ -21,24 +23,29 @@ def main():
 				cornerSegmentIds.append((nid2,nid1))
 		if len(cornerSegmentIds)<1:
 			data.addcomment('Could not find enough corner segments for way #'+str(wid))
-			break
+			continue
 		if len(cornerSegmentIds)>2:
 			data.addcomment('Found too many corner segments for way #'+str(wid))
-			break
+			continue
+		outerSegmentNid1,innerSegmentNid1=cornerSegmentIds[0]
+		outerSegmentNid2,innerSegmentNid2=cornerSegmentIds[1]
+		if innerSegmentNid1==innerSegmentNid2:
+			data.addcomment('Corner node already present for way #'+str(wid))
+			continue
 		segment1=osmcmd.Segment.fromNodes(
-			data.nodes[cornerSegmentIds[0][0]],
-			data.nodes[cornerSegmentIds[0][1]]
+			data.nodes[outerSegmentNid1],
+			data.nodes[innerSegmentNid1]
 		)
 		segment2=osmcmd.Segment.fromNodes(
-			data.nodes[cornerSegmentIds[1][0]],
-			data.nodes[cornerSegmentIds[1][1]]
+			data.nodes[outerSegmentNid2],
+			data.nodes[innerSegmentNid2]
 		)
 		x,y=segment1.intersect(segment2)
 		cornerPoint=segment1.displace(x)
 		cornerNid,cornerNode=cornerPoint.makeNode(data)
 		cornerWid=data.addway()
 		cornerWay=data.ways[cornerWid]
-		cornerWay[OsmData.REF]=[cornerSegmentIds[0][1],cornerNid,cornerSegmentIds[1][1]]
+		cornerWay[OsmData.REF]=[innerSegmentNid1,cornerNid,innerSegmentNid2]
 		data.addcomment('Restored corner for way #'+str(wid))
 	data.write(sys.stdout)
 
