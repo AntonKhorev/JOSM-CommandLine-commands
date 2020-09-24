dc2919792155fe69fe42f6ff242f8e90b349dd19
Anton Khorev <tony29@yandex.ru>
2020-09-24 03:28:25 +0300

    russian quotes for operator tags on all elements

--- poitypography.py
+++ poitypography.py
@@ -6,16 +6,23 @@ import re
 import OsmData
 import osmcmd
 
+def processPoi(poi):
+	if 'operator' in poi[OsmData.TAG]:
+		op=poi[OsmData.TAG]['operator']
+		op2,n=re.subn(r'"(.*)"',r'«\1»',op)
+		if n>0:
+			poi[OsmData.ACTION]=OsmData.MODIFY
+			poi[OsmData.TAG]['operator']=op2
+
 def main():
 	data=osmcmd.readData()
 	poidata=osmcmd.readData()
 	for id,poi in poidata.nodes.items():
-		if 'operator' in poi[OsmData.TAG]:
-			op=poi[OsmData.TAG]['operator']
-			op2,n=re.subn(r'"(.*)"',r'«\1»',op)
-			if n>0:
-				poi[OsmData.ACTION]=OsmData.MODIFY
-				poi[OsmData.TAG]['operator']=op2
+		processPoi(poi)
+	for id,poi in poidata.ways.items():
+		processPoi(poi)
+	for id,poi in poidata.relations.items():
+		processPoi(poi)
 	poidata.write(sys.stdout)
 
 if __name__=='__main__':
